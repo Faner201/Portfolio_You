68227e48dba7afd196031ccd4a80b58dd41aa768
Faner201 <62674880+Faner201@users.noreply.github.com>
2022-12-18 21:01:26 +1000

    Implementation of a database for authentication

--- auth/repository/database/user.go
+++ auth/repository/database/user.go
@@ -3,7 +3,13 @@ package database
 import (
 	"Portfolio_You/models"
 	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+type User struct {
+	ID primitive.ObjectID `bson:"_id, omitempy"`
+}
 
 type UserRepository struct{}
 
@@ -21,3 +27,42 @@ func (u UserRepository) GetUser(ctx context.Context, username, password string) 
 		Password: password,
 	}, nil
 }
+
+// type UserRepository struct {
+// 	db *mongo.Collection
+// }
+
+// func NewUserRepository(db *mongo.Database, collection string) *UserRepository {
+// 	return &UserRepository{
+// 		db: db.Collection(collection),
+// 	}
+// }
+
+// func (u UserRepository) CreateUser(ctx context.Context, user *models.User) error {
+// 	res, err := u.db.InsertOne(ctx, user)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	user.ID = res.InsertedID.(primitive.ObjectID).Hex()
+// 	return nil
+// }
+
+// func (u UserRepository) GetUser(ctx context.Context, username, password string) (*models.User, error) {
+// 	user := new(User)
+// 	modelUser := &models.User{}
+// 	err := u.db.FindOne(ctx, bson.M{
+// 		"username": username,
+// 		"password": password,
+// 	}).Decode(modelUser)
+
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return &models.User{
+// 		ID:       user.ID.Hex(),
+// 		Username: modelUser.Username,
+// 		Password: modelUser.Password,
+// 	}, nil
+// }
--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID       string `bson:"_id,omitempty"`
+	ID       string
 	Username string `bson:"username"`
 	Password string `bson:"password"`
 	Email    string `bson:"email"`
